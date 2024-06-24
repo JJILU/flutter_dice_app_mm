b1564ad055a0268eef112e1880f6bdeabbbdc339
Naomi Kapele <naomikapele@booknowzambia.com>
2024-06-24 08:15:28 +0300

    add text to show wether won or loss

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -13,12 +13,19 @@ class DiceRoller extends StatefulWidget {
 
 class _DiceRollerState extends State<DiceRoller> {
   // var activeDiceImage = "dice-images/dice-images/dice-1.png";
-  var currentDiceRoll = 2;
+  var currentDiceRoll1 = 2;
+  var currentDiceRoll2 = 1;
 
   // method
-  void rollDice() {
+  void rollDice1() {
     setState(() {
-      currentDiceRoll = Random().nextInt(6) + 1;
+      currentDiceRoll1 = Random().nextInt(6) + 1;
+    });
+  }
+
+  void rollDice2() {
+    setState(() {
+      currentDiceRoll2 = Random().nextInt(6) + 1;
     });
   }
 
@@ -27,10 +34,32 @@ class _DiceRollerState extends State<DiceRoller> {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
+        // Win or Lose Text
+        Padding(
+          padding: const EdgeInsets.only(
+            bottom: 20,
+          ),
+          child: Text(
+            currentDiceRoll1 == currentDiceRoll2 ? "You Won 😀" : "You Lost 🥺",
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 28,
+            ),
+          ),
+        ),
+
         // dice images
-        Image.asset(
-          "dice-images/dice-images/dice-$currentDiceRoll.png",
-          width: 200,
+        Row(
+          children: [
+            Image.asset(
+              "dice-images/dice-images/dice-$currentDiceRoll1.png",
+              width: 200,
+            ),
+            Image.asset(
+              "dice-images/dice-images/dice-$currentDiceRoll2.png",
+              width: 200,
+            ),
+          ],
         ),
 
         // SizedBox()
@@ -40,7 +69,10 @@ class _DiceRollerState extends State<DiceRoller> {
 
         // button
         TextButton(
-          onPressed: rollDice, // pointer to rollDice()
+          onPressed: () {
+            rollDice1();
+            rollDice2();
+          }, // pointer to rollDice()
           style: TextButton.styleFrom(
             // padding: const EdgeInsets.only(top: 20),
             foregroundColor: Colors.white,
